2b251a9411bf39611f5aaccd037527041ce2c834
VietDung7301 <vietdung.7301@gmail.com>
2022-10-03 21:52:23 +0700

    Update database URL

--- GoodFood/GoodFood.api/Consts.cs
+++ GoodFood/GoodFood.api/Consts.cs
@@ -2,6 +2,6 @@
 {
     public static class consts
     {
-        public const string dbConnection = "Server=localhost;Port=3309;Database=goodfood;Uid=root;Password=#nothingspecial";
+        public const string dbConnection = "Server=localhost;Port=3309;Database=goodfood;Uid=root;Password=password";
     }
 }
